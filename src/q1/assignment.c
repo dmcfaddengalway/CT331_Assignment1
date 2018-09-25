4378a2eb3fc40175ea68a4bec3ee4f94ba8f062a
Daniel McFadden <dmcfaddengalway@gmail.com>
2018-09-25 18:33:38 +0100

    added screenshot of assignment directions, and wrote program

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+int a;
+int* b;
+long c;
+double* d;
+char** e;
+
 int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
-  return 0;
+    printf("Hello assignment1.\n");
+    
+    printf("%d \n", sizeof(a));
+    printf("%d \n", sizeof(b));
+    printf("%d \n", sizeof(c));
+    printf("%d \n", sizeof(d));
+    printf("%d", sizeof(e));
+    
+    return 0;
 }
