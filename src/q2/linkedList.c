8f4ca3f1839604cd6e6998b2aa025e18f5eb703d
Daniel McFadden <dmcfaddengalway@gmail.com>
2018-09-25 19:03:49 +0100

    moved struct to .h file added functions to .c file added appendix type function declarations to .h file

--- src/q2/linkedList.c
+++ src/q2/linkedList.c
@@ -3,12 +3,6 @@
 #include <string.h>
 #include "linkedList.h"
 
-typedef struct listElementStruct{
-  char* data;
-  size_t size;
-  struct listElementStruct* next;
-} listElement;
-
 //Creates a new linked list element with given content of size
 //Returns a pointer to the element
 listElement* createEl(char* data, size_t size){
@@ -58,4 +52,69 @@ void deleteAfter(listElement* after){
   //need to free the memory because we used malloc
   free(delete->data);
   free(delete);
+}
+
+int length(listElement *list) {
+    
+    //Pointer to current elements's position in list
+    listElement *current = list;
+    int counter = 0;
+    
+    //While current element is not empty
+    //continue increment counter and go to next element assinging it to current
+    while (current != NULL) {
+        counter++;
+        current = current-> next;
+    }
+    
+    return printf("# of elements in the list: %d \n", counter);
+}
+
+void push(listElement **list, char *data, size_t size) {
+    
+    //Create new element and set to a pointer, then
+    //get next element and assign it to the list pointer
+    //and let the list point point to the new element created
+    listElement *newEle = createEl(data, size);
+    newEle-> next = *list;
+    *list = newEle;
+    
+}
+
+listElement *pop(listElement **list) {
+    listElement *headEle = *list;
+    
+    //If head of list*, then make the list equal to the next member
+    //of the struct the head points to (aka the next element)
+    if (headEle) {
+        *list = headEle-> next;
+    }
+    
+    return headEle;
+    
+}
+
+void enqueue(listElement **list, char *data, size_t   size) {
+    
+    //Crete new element and set to a pointer, then
+    //get next element in list and assign it to the list pointer
+    //and let the list pointer point to the new element created
+    listElement *newEle = createEl(data, size);
+    newEle-> next = *list;
+    *list = newEle;
+    
+}
+
+listElement *dequeue(listElement *list) {
+    listElement *temp = list;
+    
+    while( (temp-> next)-> next != NULL) {
+        temp = temp-> next;
+    }
+    
+    listElement *tail = temp->next;
+    temp->next = NULL;
+    
+    return tail;
+    
 }
