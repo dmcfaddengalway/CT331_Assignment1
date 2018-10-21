86b91a8ad8e21b0d8407af40213d12f996de999a
Daniel McFadden <dmcfaddengalway@gmail.com>
2018-10-21 22:51:36 +0100

    tests forther for q2

--- src/q2/tests.c
+++ src/q2/tests.c
@@ -2,8 +2,45 @@
 #include "tests.h"
 #include "linkedList.h"
 
-void stackTest() {
+void testQueue() {
+    listElement *l = createEl("Testing String (m)", 30);
+    traverse(l);
+    length(l);
     
+    enqueue(&l, "Testing String (n)", 30);
+    insertAfter(l, "Testing String (o)", 30);
+    enqueue(&l, "Testing String (p)", 30);
+    traverse(l);
+    length(l);
+    
+    dequeue(l);
+    traverse(l);
+    length(l);
+    
+    deleteAfter(l);
+    traverse(l);
+    length(l);
+    
+}
+
+void stackTest() {
+    listElement *l = createEl("Testing String (a)", 30);
+    traverse(l);
+    length(l);
+    
+    push(&l, "Testing String (c)", 30);
+    insertAfter(l, "Testign String (b)", 30);
+    push(&l, "Testing String (d)", 30);
+    traverse(l);
+    length(l);
+    
+    pop(&l);
+    traverse(l);
+    length(l);
+    
+    deleteAfter(l);
+    traverse(l);
+    length(l);
 }
 
 void runTests(){
