824b6c0b6e76945570510380993afc9ad2dc9e90
Daniel McFadden <dmcfaddengalway@gmail.com>
2018-10-21 22:47:55 +0100

    updated spacing and comments in q2

--- src/q2/linkedList.c
+++ src/q2/linkedList.c
@@ -6,52 +6,59 @@
 //Creates a new linked list element with given content of size
 //Returns a pointer to the element
 listElement* createEl(char* data, size_t size){
-  listElement* e = malloc(sizeof(listElement));
-  if(e == NULL){
-    //malloc has had an error
-    return NULL; //return NULL to indicate an error.
-  }
-  char* dataPointer = malloc(sizeof(char)*size);
-  if(dataPointer == NULL){
-    //malloc has had an error
-    free(e); //release the previously allocated memory
-    return NULL; //return NULL to indicate an error.
-  }
-  strcpy(dataPointer, data);
-  e->data = dataPointer;
-  e->size = size;
-  e->next = NULL;
-  return e;
+    listElement* e = malloc(sizeof(listElement));
+  
+    //Error in malloc
+    if(e == NULL) {
+        return NULL;
+    }
+    
+    char* dataPointer = malloc(sizeof(char)*size);
+  
+    if(dataPointer == NULL){
+        free(e);
+        return NULL;
+    }
+    
+    strcpy(dataPointer, data);
+    e->data = dataPointer;
+    e->size = size;
+    e->next = NULL;
+    
+    return e;
 }
 
 //Prints out each element in the list
 void traverse(listElement* start){
-  listElement* current = start;
-  while(current != NULL){
-    printf("%s\n", current->data);
-    current = current->next;
-  }
+    listElement* current = start;
+    
+    while(current != NULL){
+        printf("%s\n", current->data);
+        current = current->next;
+    }
+    
 }
 
 //Inserts a new element after the given el
 //Returns the pointer to the new element
 listElement* insertAfter(listElement* el, char* data, size_t size){
-  listElement* newEl = createEl(data, size);
-  listElement* next = el->next;
-  newEl->next = next;
-  el->next = newEl;
-  return newEl;
+    listElement* newEl = createEl(data, size);
+    listElement* next = el->next;
+    newEl->next = next;
+    el->next = newEl;
+  
+    return newEl;
 }
 
 
 //Delete the element after the given el
 void deleteAfter(listElement* after){
-  listElement* delete = after->next;
-  listElement* newNext = delete->next;
-  after->next = newNext;
-  //need to free the memory because we used malloc
-  free(delete->data);
-  free(delete);
+    listElement* delete = after->next;
+    listElement* newNext = delete->next;
+    after->next = newNext;
+
+    free(delete->data);
+    free(delete);
 }
 
 int length(listElement *list) {
